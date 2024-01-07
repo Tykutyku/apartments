1ff44992d4803cfe14aa75df37b6252ea9f34ba2
Kornelia Zarębińska <71494339+Tykutyku@users.noreply.github.com>
2024-01-08 00:33:49 +0100

    End of section 12

--- src/App/Controllers/HomeController.php
+++ src/App/Controllers/HomeController.php
@@ -11,12 +11,12 @@ class HomeController
 {
     public function __construct(private TemplateEngine $view)
     {
+        var_dump($this->view);
+        echo "<br>";
     }
 
     public function home()
     {
-        echo $this->view->render("/index.php", [
-            'title' => 'Home page'
-        ]);
+        echo $this->view->render("index.php");
     }
 }
